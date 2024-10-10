a763259acc1575de5270dbb9bc73cc37c488c7ac
ZinebSG <zaineb.soudi@gmail.com>
2024-10-10 14:42:55 +0200

    update choice adding juce as the 4th choice

--- choixcafe.sh
+++ choixcafe.sh
@@ -13,7 +13,7 @@ while true; do
     1) echo "Voici votre café." ;;
     2) echo "Voici votre thé." ;;
     3) echo "Voici votre chocolat." ;;
-    4) echo "Voici votre juce." ;;
+    4) echo "Voici votre jus." ;;
     x) echo "Au revoir !" ; break ;;
     *) echo "Choix invalide." ;;
   esac
